2e3a9030cc51df50e836816c040f0bbb7d79b3e3
Jones <michaeljones401@gmail.com>
2021-12-02 12:15:29 +0000

    Update Greeter.php
    testing

--- src/Greeter.php
+++ src/Greeter.php
@@ -4,9 +4,9 @@ namespace App;
 class Greeter {
 	public function greet(string $name = null):string {
 		if($name) {
-			return "Hello, $name!";
+			return "Hello, $name!b";
 		}
 
-		return "Hello!";
+		return "Hello!b";
 	}
 }
