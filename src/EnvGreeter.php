d4c9cc38e4280f9be31fb86d4f3b117a769d793a
Jones <michaeljones401@gmail.com>
2021-12-02 12:26:05 +0000

    Update EnvGreeter.php

--- src/EnvGreeter.php
+++ src/EnvGreeter.php
@@ -5,5 +5,6 @@ class EnvGreeter extends Greeter {
 	public function greetFromEnv(string $environmentVariableName):string {
 		$name = getenv($environmentVariableName);
 		return $this->greet($name);
+		
 	}
 }
